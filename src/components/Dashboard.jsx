49430ba28dd4e06cbaf07954ee4d6529542405fe
Alexis <amaryllisno@gmail.com>
2021-08-23 19:26:32 +0200

    fix: redirect when logged out

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,51 @@
+import { useState } from 'react';
+import { Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../context/AuthContext';
+import { Link, useHistory, Redirect } from 'react-router-dom';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [error, setError] = useState('');
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
+
+  const handleLogout = async () => {
+    setError('');
+    try {
+      await logout();
+      history.push('login');
+    } catch {
+      setError('failed to log out ');
+    }
+  };
+
+  if (currentUser === null) {
+    return <Redirect to='/login'></Redirect>;
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>Profile</h2>
+          {error && <Alert variant='danger'>{error}</Alert>}
+          {currentUser && (
+            <div>
+              <strong>Email: </strong> {currentUser.email}
+            </div>
+          )}
+
+          <Link to='/update-profile' className='btn btn-primary w-100 mt-3'>
+            Update Profile
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className='w-100 text-center mt-2'>
+        <Button variant='link' onClick={handleLogout}>
+          Log out
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
